c734b988bbc626aae6854d7de446e01d20f97da4
Sergio Placencia <buyworld61@gmail.com>
2026-03-24 23:54:18 -0400

    fix: define node version

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,6 @@ import { env } from "./env"
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando" })
+})
