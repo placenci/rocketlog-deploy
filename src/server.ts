d75a85f6a491d70d058474f4500205d9e7ec3d57
Sergio Placencia <buyworld61@gmail.com>
2026-03-24 20:52:45 -0400

    fix: define node version

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "@/app"
 import { env } from "./env" 
 
-const PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
